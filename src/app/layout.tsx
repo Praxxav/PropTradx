e43e7e0810c997dab62f086b7d807042ee676ddb
unknown <pranavkulkarni33917@gmail.com>
2025-05-31 12:30:18 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,18 +16,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${inter.variable} ${geistMono.variable}`} suppressHydrationWarning>
-      <head>
-         <Script>
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5355978764450307"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-        </Script>
-      </head>
+  
       <body className="bg-gray-50 text-gray-950 dark:bg-gray-900 dark:text-white min-h-screen">
        <SessionProviderWrapper>
         <LayoutWrapper>{children}</LayoutWrapper>
         </SessionProviderWrapper>
+        
         {/* Google Analytics Script */}
          <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-W9F5DFXMSV"
